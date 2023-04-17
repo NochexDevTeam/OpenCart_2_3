78867808650170030845889bd020a84cc7c00cc1
NochexDevTeam <36759878+NochexDevTeam@users.noreply.github.com>
2023-04-17 09:58:37 +0100

    Update nochex.php

--- catalog/controller/extension/payment/nochex.php
+++ catalog/controller/extension/payment/nochex.php
@@ -230,7 +230,7 @@ class ControllerExtensionPaymentNochex extends Controller {
 
 }else{
 	
-		$url = "https://www.nochex.com/apcnet/apc.aspx";
+		$url = "https://secure.nochex.com/apc/apc.aspx";
 
 		// Curl code to post variables back
 		$ch = curl_init(); // Initialise the curl tranfer
